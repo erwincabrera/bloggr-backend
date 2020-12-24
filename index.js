9b1dc131d8fbcff69f7a2731addf4da15f59d575
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2020-12-24 16:44:56 +0800

    Update port

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = require('./app')
 
 const server = http.createServer(app)
 
-server.listen(config.PORT, () => {
-    logger.info(`Server running on port ${config.PORT}`)
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+    logger.info(`Server running on port ${PORT}`)
 })
